0578e48462787ae02da56c25c5e9d31bf0cd0f58
Levi <levi@coassemble.com>
2022-05-04 12:59:44 +1000

    Set plugin ui height

--- figma/code.ts
+++ figma/code.ts
@@ -8,7 +8,9 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__)
+figma.showUI(__html__, {
+    height: 400
+})
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
