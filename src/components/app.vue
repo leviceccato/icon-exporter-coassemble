d72c468bfefbf9df652cd590ead2a327e17d9cb3
Levi <levi@coassemble.com>
2022-05-06 10:58:05 +1000

    Optimise font and css

--- src/components/app.vue
+++ src/components/app.vue
@@ -261,8 +261,16 @@ watch(() => state.event, event => {
 </template>
 
 <style lang="scss">
-@import 'figma-plugin-ds/dist/figma-plugin-ds.css';
-@import 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap';
+@use 'figma-plugin-ds/dist/figma-plugin-ds.css';
+
+@font-face {
+    font-family: 'Roboto Mono';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/robotomono/v21/L0xuDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_3vq_ROW-AJi8SJQt.woff) format('woff');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 
 .type--fade { color: var(--black1); }
 .type--mono { font-family: 'Roboto Mono', monospace; }
